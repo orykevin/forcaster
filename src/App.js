d3838e01b8ac514dac5718b440900503363eedf5
Ory Kevin <kevinory2020@gmail.com>
2022-07-14 20:08:48 +0700

    fixing lat n long

--- src/App.js
+++ src/App.js
@@ -13,11 +13,11 @@ function App() {
 
   const location = useGeoLocation()
   //console.log(location.cordinates.lat)
-  /*const {data,loading,error} = useFetch(`https://api.openweathermap.org/data/2.5/onecall?lat=${location.cordinates.lat}&lon=${location.cordinates.long}&appid=814f744a41d74fc77cc944f47c2c107c&units=metric&lang=en`)
+  const {data,loading,error} = useFetch(`https://api.openweathermap.org/data/2.5/onecall?lat=${location.cordinates.lat}&lon=${location.cordinates.long}&appid=814f744a41d74fc77cc944f47c2c107c&units=metric&lang=en`)
   const {data1,loading1,error1} = useFetch2(`https://nominatim.openstreetmap.org/reverse?format=json&lat=${location.cordinates.lat}&lon=${location.cordinates.long}&zoom=18&addressdetails=1`)
-  */
-  const {data,loading,error} = useFetch(`https://api.openweathermap.org/data/2.5/onecall?lat=64.128288&lon=-21.827774&appid=814f744a41d74fc77cc944f47c2c107c&units=metric&lang=en`)
-  const {data1,loading1,error1} = useFetch2(`https://nominatim.openstreetmap.org/reverse?format=json&lat=64.128288&lon=-21.827774&zoom=18&addressdetails=1`)
+  
+  /*const {data,loading,error} = useFetch(`https://api.openweathermap.org/data/2.5/onecall?lat=64.128288&lon=-21.827774&appid=814f744a41d74fc77cc944f47c2c107c&units=metric&lang=en`)
+  const {data1,loading1,error1} = useFetch2(`https://nominatim.openstreetmap.org/reverse?format=json&lat=64.128288&lon=-21.827774&zoom=18&addressdetails=1`)*/
   //if (data) console.log(data)
   //if (data1) console.log(data1.data.address)
   let currentWeather = {}
